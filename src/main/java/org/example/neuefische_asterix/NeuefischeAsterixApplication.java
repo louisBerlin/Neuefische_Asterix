010c349badd1063f74ce6200fc13f5327527a6af
louisBerlin <louis.froch@gmail.com>
2024-04-03 16:11:08 +0200

    Initial commit

---
+++ src/main/java/org/example/neuefische_asterix/NeuefischeAsterixApplication.java
@@ -0,0 +1,13 @@
+package org.example.neuefische_asterix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeAsterixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuefischeAsterixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/neuefische_asterix/NeuefischeAsterixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.neuefische_asterix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeAsterixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
